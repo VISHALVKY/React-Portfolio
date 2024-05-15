ecc5d17ecf7b497bc5e27b0afd4f37478f02758c
VISHALVKY <vishalvky1510@gmail.com>
2024-05-15 11:54:26 +0530

    first commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,69 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"Quiz App using Images",
+        tags:["react","javascript","styled-components"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://ik.imagekit.io/ably/ghost/prod/2023/11/best-react-component-libraries.png?tr=w-1728,q-50",
+        link:"https://github.com/VISHALVKY/Abans-Quizz-App"
+    },
+    {
+        id:2,
+        name:"Burger Shop",
+        tags:["HTML","CSS","Bootstrap"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://i0.wp.com/tolustar.com/wp-content/uploads/2020/02/html-css.jpg?fit=750%2C422&ssl=1",
+        link:"https://github.com/VISHALVKY/Burger-Shop"
+    },
+    {
+        id:3,
+        name:"OTP Generator",
+        tags:["HTML","CSS","Bootstrap"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://i0.wp.com/tolustar.com/wp-content/uploads/2020/02/html-css.jpg?fit=750%2C422&ssl=1",
+        link:"https://github.com/VISHALVKY/4-Digit-OTP"
+    },
+    {
+        id:4,
+        name:"Project On StockMarket",
+        tags:["react","API","styled-components"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://t3.ftcdn.net/jpg/04/57/12/80/360_F_457128092_6kyDswhxtRySUSfqsLp2Ul8DKucUH9Sc.jpg",
+        link:"https://github.com/VISHALVKY/Stock-Market-API"
+    },
+    {
+        id:5,
+        name:"Glassmorphism",
+        tags:["HTML","CSS","JavaScript"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://t3.ftcdn.net/jpg/03/21/24/30/360_F_321243084_GstfWflk1eTLlzUdRZ5mjoP5IG1iCc8J.jpg",
+        link:"https://github.com/VISHALVKY/Glassmorphism",
+    },
+    {
+        id:6,
+        name:"Stone-Paper-Scissors Game",
+        tags:["react","css","styled-components"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://ik.imagekit.io/ably/ghost/prod/2023/11/best-react-component-libraries.png?tr=w-1728,q-50",
+        link:"https://github.com/VISHALVKY/Stone-Paper-Scissors-Game"
+
+    },
+   
+    {
+        id:7,
+        name:"Display Current date & Time",
+        tags:["react","styled-components","javascript"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://t3.ftcdn.net/jpg/04/57/12/80/360_F_457128092_6kyDswhxtRySUSfqsLp2Ul8DKucUH9Sc.jpg",
+        link:"https://github.com/VISHALVKY/Date-Time-React"
+    },
+    {
+        id:8,
+        name:"Music player",
+        tags:["HTML","CSS","JavaScript"],
+        date:"Click on the image to get more Info",
+        imgSrc:"https://t3.ftcdn.net/jpg/03/21/24/30/360_F_321243084_GstfWflk1eTLlzUdRZ5mjoP5IG1iCc8J.jpg",
+        link:"https://github.com/VISHALVKY/music-player-slider"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+export const Work = [
+    {
+        id:1,
+        name:"JSON CRUD Operation",
+        description:"The application is built with a responsive design, ensuring optimal performance and user experience across various devices.",
+        tags:["react","styledComponents","API"],
+        
+        demo:"https://json-crud-react.netlify.app/",
+        github:"https://github.com/VISHALVKY/JSON-CRUD-Using-React"
+    },
+    {
+        id:2,
+        name:"QR Code Generator",
+        description:"Users can generate QR codes dynamically by inputting text, URLs, or any other data they want to encode.",
+        tags:["react","styledComponents"],
+        
+        demo:"https://qrgenerator-react.netlify.app/",
+        github:"https://github.com/VISHALVKY/QR-Code-React"
+        
+    },
+    {
+        id:3,
+        name:"ToDo-App",
+        description:"Effortlessly manage your to-do list and boost your productivity with the React Todo App.",
+        tags:["react","styledComponents"],
+        
+        demo:"https://simpletodolist-react.netlify.app/",
+        github:"https://github.com/VISHALVKY/TodoList-React"
+    },
+    {
+        id:4,
+        name:"Tic Tac Toe",
+        description:"Play solo or against a friend, with real-time updates and responsive design for seamless gaming on any device.",
+        tags:["react","styledComponents"],
+        
+        demo:"https://tic-tac-toe-gamesreact.netlify.app/",
+        github:"https://github.com/VISHALVKY/Tic-Tac-Toe"
+       
+    },{
+        id:5,
+        name:"Amazon Clone",
+        description:"Explore the familiar layout and design of Amazon with our HTML & CSS replica. From the iconic logo to the detailed product listings.",
+        tags:["HTML","CSS"],
+        
+        demo:"https://amazone-cloneapp.netlify.app/",
+        github:"https://github.com/VISHALVKY/amazon-clone"
+        
+    },
+    {
+        id:6,
+        name:"Weather App",
+        description:"WeatherWise is your personal weather companion, delivering accurate forecasts right to your fingertips.",
+        tags:["HTML","CSS","API"],
+        
+        demo:"https://weather-condition-app-api.netlify.app/",
+        github:"https://github.com/VISHALVKY/Weather-web-app"
+        
+    },
+    
+  
+] 
